bf50b4962aebcdec5ea7fd1bbc833ec54372327e
Onkar Garde <158274484+Omkargarde123@users.noreply.github.com>
2026-07-28 22:44:38 +0530

    Time: 23 ms (82.86%), Space: 73.9 MB (43.74%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        vector<int> freq(26, 0);
+
+        for (char c : s)
+            freq[c - 'a']++;
+
+        string first = "";
+        char mid = 0;
+
+        for (int i = 0; i < 26; i++) {
+            first.append(freq[i] / 2, char('a' + i));
+            if (freq[i] % 2)
+                mid = char('a' + i);
+        }
+
+        string second = first;
+        reverse(second.begin(), second.end());
+
+        if (mid)
+            return first + mid + second;
+
+        return first + second;
+    }
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1) (26-character frequency array)
