eb1451c17b8f46682ae846fa4c27bee3c50b643a
Onkar Garde <158274484+Omkargarde123@users.noreply.github.com>
2026-08-18 17:25:47 +0530

    Time: 3 ms (54.14%), Space: 29.4 MB (51.5%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int largestInteger(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        std::unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Case 1: k == 1
+        if (k == 1) {
+            int ans = -1;
+            for (auto& [num, count] : freq) {
+                if (count == 1) {
+                    ans = std::max(ans, num);
+                }
+            }
+            return ans;
+        }
+
+        // Case 2: k == n
+        if (k == n) {
+            int ans = -1;
+            for (int num : nums) {
+                ans = std::max(ans, num);
+            }
+            return ans;
+        }
+
+        // Case 3: 1 < k < n
+        int ans = -1;
+        if (freq[nums[0]] == 1) {
+            ans = std::max(ans, nums[0]);
+        }
+        if (freq[nums[n - 1]] == 1) {
+            ans = std::max(ans, nums[n - 1]);
+        }
+
+        return ans;
+    }
+};
